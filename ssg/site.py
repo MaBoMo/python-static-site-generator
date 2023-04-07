a95e7a6e2652b2566cc873bd6f1fd3283151d924
Moritz Maier-Borst <moritz.maier-borst@sap.com>
2023-04-07 11:51:14 +0200

    update site class

--- ssg/site.py
+++ ssg/site.py
@@ -9,9 +9,11 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-        directory.makedir(parents=True, exist_ok=True)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+
         for path in self.source.rglob("*"):
             if os.path.isdir(path):
                 self.create_dir(path)
